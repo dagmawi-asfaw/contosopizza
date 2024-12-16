bc27c4566b18d1edd0e78c2adfc363fc5fb6fa75
dagmawi-asfaw <71550160+dagmawi-asfaw@users.noreply.github.com>
2024-12-16 08:54:39 +0300

    feat: add sauce, topping models and refactor pizza model
    - remove unused weather forecast controller and service

--- models/pizza.cs
+++ models/pizza.cs
@@ -4,5 +4,8 @@ public class Pizza
 {
     public int Id { get; set; }
     public string? Name { get; set; }
-    public bool IsGlutenFree { get; set; }
+
+    public Sauce? Sauce { get; set; }
+
+    public ICollection<Topping>? Toppings { get; set; }
 }
---
+++ models/sauce.cs
@@ -0,0 +1,7 @@
+namespace contosopizza.Models;
+
+public class Sauce{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+}
